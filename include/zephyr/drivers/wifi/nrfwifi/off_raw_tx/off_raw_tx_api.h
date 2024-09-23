c7d0bd8690ce1efd559dbdcb6e15bf4cdaae7ed5
Sachin D Kulkarni <sachin.kulkarni@nordicsemi.no>
2024-09-23 18:39:44 +0530

    wifi: Add APIs for offloaded raw tx feature
    Add APIs for the offloaded raw tx feature supported by nRF70 devices.
    
    Signed-off-by: Sachin D Kulkarni <sachin.kulkarni@nordicsemi.no>

---
+++ include/zephyr/drivers/wifi/nrfwifi/off_raw_tx/off_raw_tx_api.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @brief File containing API's for the Offloaded raw TX feature.
+ */
+
+#include <stdlib.h>
